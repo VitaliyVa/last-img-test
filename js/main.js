115041d8e03cc2dcc3cf28903b2db63be3ae6d78
VitaliyVa <vistet1428@gmail.com>
2019-10-07 14:44:56 +0300

    done

---
+++ js/main.js
@@ -0,0 +1,33 @@
+var wow = new WOW(
+  {
+    boxClass:     'block-an',      // animated element css class (default is wow)
+    animateClass: 'box', // animation css class (default is animated)
+    offset:       110,          // distance to the element when triggering the animation (default is 0)
+    mobile:       true,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    callback:     function(box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null,    // optional scroll container selector, otherwise use window,
+    resetAnimation: true,     // reset animation on end (default is true)
+  }
+);
+wow.init();
+var wow = new WOW(
+  {
+    boxClass:     'ttt',      // animated element css class (default is wow)
+    animateClass: 'text', // animation css class (default is animated)
+    offset:       110,          // distance to the element when triggering the animation (default is 0)
+    mobile:       true,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    callback:     function(box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null,    // optional scroll container selector, otherwise use window,
+    resetAnimation: true,     // reset animation on end (default is true)
+  }
+);
+
+wow.init();
